d85b794d2b086c39755b4f2131ece8b441d8520b
kebernet <rcooper@thoughtworks.com>
2024-07-31 11:59:51 -0400

    Changing mac-arm JDK to see if it fixed the build problem.

--- src/main/java/com/thoughtworks/gha/notifier/service/MonitorService.java
+++ src/main/java/com/thoughtworks/gha/notifier/service/MonitorService.java
@@ -11,7 +11,6 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Timer;
